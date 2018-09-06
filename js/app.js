14a796abb5cbf6c1a03ecb38f1d6263b91d7aff0
Ali Akbar <ali.akbar102030@gmail.com>
2018-09-06 15:25:35 +0500

    ajax added to my javascript

---
+++ js/app.js
@@ -0,0 +1,14 @@
+(function IIFE($) {
+  let load = $("#load"),
+    nav = $(".nav"),
+    classNames = [".btn--success", ".btn--info"];
+
+  $(nav).on("click", "a", function (e) {
+    let successBtn = $(classNames[0]);
+    let target = $(e.target),
+      link = target.attr("href");
+    e.preventDefault();
+
+    load.stop().empty().load(link);
+  });
+}(jQuery));
